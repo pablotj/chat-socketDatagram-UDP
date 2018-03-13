6cca1c1237a7edddaebd6f3bafeedb92a04c67f2
pablotj <pablodelatorree@gmail.com>
2018-03-13 21:46:09 +0100

    Modified toString

--- Client/src/app/Message.kt
+++ Client/src/app/Message.kt
@@ -3,11 +3,14 @@ package app
 import java.io.Serializable
 import java.util.*
 
-data class Message(var userName: String, var message: String, var date: Date, var hostname: String, var host: Int, var status: Boolean): Serializable{
+data class Message(var userName: String, var message: String, var date: Date, var hostname: String, var host: Int,  var status: Boolean): Serializable{
     override fun toString(): String {
-        return "$userName on $date \n$message\n"
-    }
-    fun toStringOther():String{
-        return "$userName $message\n"
+        val rtn: String
+        if(status){
+            rtn = "$userName $message\n"
+        }else{
+            rtn = "$userName on $date \n$message\n"
+        }
+        return rtn
     }
 }
--- Server/src/app/Message.kt
+++ Server/src/app/Message.kt
@@ -5,9 +5,12 @@ import java.util.*
 
 data class Message(var userName: String, var message: String, var date: Date, var hostname: String, var host: Int,  var status: Boolean): Serializable{
     override fun toString(): String {
-        return "$userName on $date \n$message\n"
-    }
-    fun toStringOther():String{
-        return "$userName $message\n"
+        val rtn: String
+        if(status){
+            rtn = "$userName $message\n"
+        }else{
+            rtn = "$userName on $date \n$message\n"
+        }
+        return rtn
     }
 }
