e4f695a1709dd4ca5edfc9d4d12a09d76f70bbbb
pablotj <pablodelatorree@gmail.com>
2018-03-14 11:01:55 +0100

    Updated interface

--- Client/src/app/Main.kt
+++ Client/src/app/Main.kt
@@ -1,5 +1,6 @@
 package app
 
+import java.util.*
 import javax.swing.JOptionPane
 
 object Main{
@@ -8,7 +9,8 @@ object Main{
     fun main(args: Array<String>) {
         val serverHostname= "192.168.0.4"
         val serverPort= 8008
-        val session = JOptionPane.showInputDialog("WELCOME TO OPEN CHAT\n Input your user name for began")
+       // val session = JOptionPane.showInputDialog("WELCOME TO OPEN CHAT\n Enter a username to enter the chat")
+        val session = (Random().nextInt(999999999-111111111 ) + 111111111).toString()
         Socket(session,serverHostname,serverPort ).start()
 
     }
--- Client/src/app/View.kt
+++ Client/src/app/View.kt
@@ -1,12 +1,17 @@
 package app
 
-import java.awt.Color
-import java.awt.Font
+import java.awt.*
 import java.awt.event.*
+import java.text.SimpleDateFormat
 import javax.swing.*
 import java.util.*
 import javax.swing.JScrollPane
 import javax.swing.JTextArea
+import javax.swing.Spring.height
+import com.sun.awt.SecurityWarning.getSize
+import java.awt.Dimension
+
+
 
 
 
@@ -15,84 +20,129 @@ import javax.swing.JTextArea
  *
  * @author Pablo
  */
-class View(var trn: Socket) : JFrame(){
-
-    private var jtConversation: JTextArea? = JTextArea()
+class View(var socket: Socket) : JFrame(){
+    private var nameUser: String?= null
+    private var jtConversation: JEditorPane? = null
     private var jsConversation: JScrollPane? = null
-    private var jtMessage: JTextField? = null
+    private var jtMessage: JTextArea? = null
+    private var jsMessage: JScrollPane? = null
     private var jbSend: JButton? = null
 
-
-
     init {
+        nameUser = socket.nameUser
+        components()
+        window()
+    }
 
-        init()
+    private fun components() {
 
+        jtConversation = JEditorPane()
+        jtConversation!!.setBounds(0, 0, 250, 400)
+        jtConversation!!.isEditable = false
+
+        //jtConversation!!.lineWrap = true
+        jtConversation!!.font = Font("Optima", Font.PLAIN, 13)
+
+        jsConversation= JScrollPane(jtConversation)
+        jsConversation!!.setBounds(0, 0, 250, 300)
+        jsConversation!!.verticalScrollBarPolicy = JScrollPane.VERTICAL_SCROLLBAR_ALWAYS
+
+        add(jsConversation)
+
+        jtMessage = JTextArea("Write your message ...")
+        jtMessage!!.setBounds(0,300,200,50)
+        jtMessage!!.font = Font("Optima", Font.PLAIN, 13)
+        jtMessage!!.addMouseListener(object : MouseAdapter() {
+            override fun mouseClicked(e: MouseEvent?) {
+                jtMessage!!.text=""
+            }
+        })
+        jtMessage!!.lineWrap = true
+
+
+        jsMessage= JScrollPane(jtMessage)
+        jsMessage!!.setBounds(0, 300, 200, 50)
+        jsMessage!!.verticalScrollBarPolicy = JScrollPane.VERTICAL_SCROLLBAR_ALWAYS
+        add(jsMessage)
+
+        jbSend = JButton("Send")
+        jbSend!!.setBounds(200,300,50,50)
+        jbSend!!.addActionListener(ClickSend())
+        add(jbSend)
+        var jLabel = JLabel("User Name")
+        jLabel.setBounds(0, 350, 100, 25)
+        add(jLabel)
+        var jTextField = JTextField()
+        jTextField.setBounds(100, 350, 100, 25)
+        jTextField.addActionListener { jTextField.text="" }
+        add(jTextField)
+
+        var jChange = JButton("SET")
+        jChange.setBounds(200, 350, 50, 25)
+        jChange.addActionListener { nameUser = jTextField.text
+            title = "Chat of * ${nameUser!!.toUpperCase()} *"}
+        add(jChange)
+
+
+    }
+    var x : String = ""
+    private fun window(){
         defaultCloseOperation = JFrame.DO_NOTHING_ON_CLOSE
         addWindowListener(object : WindowAdapter() {
             override fun windowClosing(windowEvent: WindowEvent?) {
-                trn.send(Message(trn.nameUser, "has left the group", Date(),trn.hostname!!, trn.port!!,false))
+                socket.send(Message(nameUser!!, "has left the group", Date(),socket.hostname!!, socket.port!!,false))
                 System.exit(0)
             }
         })
-        setSize(500, 350)
+        setSize(250, 400)
         setLocationRelativeTo(null)
+
         layout = null
         isResizable = false
         isVisible = true
-        title = "Chat of * ${trn.nameUser.toUpperCase()} *"
-
-
+        title = "Chat of * ${socket.nameUser.toUpperCase()} *"
 
     }
-    private fun init() {
-
-        jtConversation = JTextArea()
-        jtConversation!!.setBounds(0, 0, 500, 250)
-        jtConversation!!.background = Color.white
-        jtConversation!!.font = Font("Arial", Font.PLAIN, 13)
-
-        jsConversation= JScrollPane(jtConversation)
-        jsConversation!!.setBounds(0, 0, 500, 250)
-        jsConversation!!.verticalScrollBarPolicy = JScrollPane.VERTICAL_SCROLLBAR_ALWAYS
-        add(jsConversation)
-
-        jtMessage = JTextField("Write your message ...")
-        jtMessage!!.setBounds(0,250,350,50)
-        jtMessage!!.addActionListener (ClickSend())
-        jtMessage!!.addMouseListener(object : MouseAdapter() {
-            override fun mouseClicked(e: MouseEvent?) {
-                jtMessage!!.text = ""
-            }
-        })
-        add(jtMessage)
-
-        jbSend = JButton("Send")
-        jbSend!!.setBounds(350,250,100,50)
-        jbSend!!.addActionListener(ClickSend())
-        add(jbSend)
-    }
-
     fun write(message: Message){
 
         jtMessage!!.text = ""
 
         if(message.message=="has left the group" ||message.message=="has just joined the group"){
-            jtConversation!!.append(message.toString())
+            jtConversation!!.contentType = "text/html"
+            x+="<p style='width:200dp; color:gray; text-align:center;'><i><b>${message}</b><i></p>"
+            jtConversation!!.text = x
+            move()
+
+
         }else{
-            jtConversation!!.append(message.toString())
+            if(socket.port!=message.host){
+            jtConversation!!.contentType = "text/html"
+            x+="<p style='width:200dp;'><b style='color:blue;'>${message.userName.toUpperCase()}</b> <a style='color:gray; '>at ${SimpleDateFormat("HH:mm dd/MM/yyyy").format(message.date)}</a></p> <p  style='color:blue; word-wrap: break-word;'>${message.message}</p>"
+            jtConversation!!.text = x
+            move()}
 
+            else{
+                jtConversation!!.contentType = "text/html"
+                x+="<p style='width:200dp; text-align:right;'><b style='color:green;'>${message.userName.toUpperCase()}</b> <a style='color:gray; '>at ${SimpleDateFormat("HH:mm dd/MM/yyyy").format(message.date)}</a></p> <p style='color:green; text-align:right; word-wrap: break-word;'>${message.message}</p>"
+                jtConversation!!.text = x
+                move()
+            }
         }
-
-
     }
+fun move(){
 
+    val tamanhoTextArea = jtConversation!!.size
+    val p = Point(
+            0,
+            tamanhoTextArea.height)
+    jsConversation!!.viewport.viewPosition = p
+}
 
     internal inner class ClickSend : ActionListener {
 
         override fun actionPerformed(e: ActionEvent) {
 
-             trn.send(Message(trn.nameUser,jtMessage!!.text, Date(),trn.hostname!!, trn.port!!,true))
+             socket.send(Message(nameUser!!,jtMessage!!.text, Date(),socket.hostname!!, socket.port!!,true))
         }
     }
 
